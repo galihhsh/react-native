0807f45f503a8073dda26be47c3efd395203eb38
galihhsh <g47ihsaputra@gmail.com>
2024-01-20 16:13:54 +0700

    first commit

---
+++ src/config/FIREBASE/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyABj-lMCI6fDvPXWj35AVzhtq8idUr_Rs0',
+  authDomain: 'crud-f11ce.firebaseapp.com',
+  projectId: 'crud-f11ce',
+  storageBucket: 'crud-f11ce.appspot.com',
+  messagingSenderId: '292143108146',
+  appId: '1:292143108146:web:2a2318807786c01b7fac2b',
+  measurementId: 'G-4B5EDLSDVC',
+});
+
+const FIREBASE = firebase;
+
+export default FIREBASE;
